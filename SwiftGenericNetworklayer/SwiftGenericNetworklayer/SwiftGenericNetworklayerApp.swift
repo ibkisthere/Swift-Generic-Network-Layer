e5428d67fa7b9135e877462a1d7b533116ed2e6e
ibkisthere <ibukunoluwaakintobi@gmail.com>
2024-06-06 05:55:30 +0100

    chore:initialized new xcode project

---
+++ SwiftGenericNetworklayer/SwiftGenericNetworklayer/SwiftGenericNetworklayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftGenericNetworklayerApp.swift
+//  SwiftGenericNetworklayer
+//
+//  Created by Ibukunoluwa Akintobi on 06/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftGenericNetworklayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
